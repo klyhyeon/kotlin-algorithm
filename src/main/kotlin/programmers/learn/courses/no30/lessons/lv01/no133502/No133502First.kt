614f36ee16dfff5c557530500fdf476a79229ac6
baekho <klyhyeon@gmail.com>
2024-01-03 23:08:57 +0900

    feat 햄버거 만들기

---
+++ src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no133502/No133502First.kt
@@ -0,0 +1,18 @@
+package programmers.learn.courses.no30.lessons.lv01.no133502
+
+/* * 햄버거 만들기
+* - 실패(모르겠다)
+* */
+class No133502First {
+
+    fun solution(ingredient: IntArray): Int {
+        var answer: Int = 0
+        // 1,2,3 연속된 index를 remove?
+
+        return answer
+    }
+}
+
+fun main() {
+
+}
