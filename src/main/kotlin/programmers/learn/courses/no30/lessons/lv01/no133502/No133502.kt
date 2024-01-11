d32eb9e4a891fd473e45020b1b280b275004f3aa
baekho <klyhyeon@gmail.com>
2024-01-11 23:57:48 +0900

    feat 햄버거 만들기 두번째 풀기

--- src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no133502/No133502.kt
+++ src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no133502/No133502.kt
@@ -8,6 +8,8 @@ import java.util.LinkedList
 * */
 class No133502 {
 
+    // 1,2,3,1
+
     fun solution(ingredient: IntArray): Int {
         var answer: Int = 0
         // 1,2,3 연속된 index를 remove?
@@ -17,9 +19,10 @@ class No133502 {
         while (i < linkedList.size) {
             if (i < linkedList.size - 3) {
                 if (linkedList[i] == 1 && linkedList[i + 1] == 2 &&
-                    linkedList[i + 2] == 3 && linkedList[i + 3] == 1) {
+                    linkedList[i + 2] == 3 && linkedList[i + 3] == 1
+                ) {
                     answer += 1
-                    for (j in 1 .. 4) {
+                    for (j in 1..4) {
                         linkedList.removeAt(i)
                     }
                     i = 0.coerceAtLeast(i - 3)
@@ -32,8 +35,25 @@ class No133502 {
         }
         return answer
     }
+
+    fun solution2(ingredient: IntArray): Int {
+        var answer: Int = 0
+        val linkedList: LinkedList<Int> = LinkedList()
+        for (i in ingredient) {
+            val size = linkedList.size
+            if (i == 1 && size >= 3) {
+                if (linkedList[size - 1] == 3 && linkedList[size - 2] == 2 && linkedList[size - 3] == 1) {
+                    repeat(3) { linkedList.pollLast() }
+                    ++answer
+                    continue
+                }
+            }
+            linkedList.offer(i)
+        }
+        return answer
+    }
 }
 
 fun main() {
-    println(No133502().solution(intArrayOf(1, 3, 2, 1, 2, 1, 3, 1, 2)))
+    println(No133502().solution2(intArrayOf(2, 1, 1, 2, 3, 1, 2, 3, 1)))
 }
