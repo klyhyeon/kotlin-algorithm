e2c0a074379e4fdee87065570ff4763edede4746
baekho <klyhyeon@gmail.com>
2024-01-18 23:49:25 +0900

    feat 카카오 신규 아이디 추천 다른 풀이 더 느림

--- src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no72410/No72410.kt
+++ src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no72410/No72410.kt
@@ -38,9 +38,44 @@ class No72410 {
         return answer
     }
 
+    // 더 느림
+    fun solution2(new_id: String): String {
+        var answer: StringBuilder = StringBuilder()
+        // step 1
+        answer.append(new_id.lowercase())
+        // step 2
+        answer.replace("[^A-Za-z0-9.\\-_]".toRegex(), "")
+        // step 3
+        while (answer.contains("..")) {
+            answer.replace("..".toRegex(), ".")
+        }
+        // step 4
+        while (answer.startsWith(".")) {
+            answer.removePrefix(".")
+        }
+        while (answer.endsWith(".")) {
+            answer.removeSuffix(".")
+        }
+        // step 5
+        if (answer.isEmpty()) answer.append("a")
+        // step 6
+        if (answer.length >= 16) answer.substring(0, 15)
+        while (answer.endsWith(".")) {
+            answer.removeSuffix(".")
+        }
+        // step 7
+        if (answer.length <= 2) {
+            while (answer.length <= 2) {
+                answer.append(answer.last())
+            }
+        }
+        return answer.toString()
+    }
+
 }
 
 fun main() {
     var newId = "abcdefghijklmn.p"
-    println(No72410().solution(newId))
+//    println(No72410().solution(newId))
+    println(No72410().solution2(newId))
 }
