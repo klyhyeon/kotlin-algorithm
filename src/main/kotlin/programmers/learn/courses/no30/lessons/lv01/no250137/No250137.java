396d660163f497c5ef68399373331899f00aed37
baekho <klyhyeon@gmail.com>
2024-04-26 07:15:14 +0900

    WIP 체력 회복

---
+++ src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no250137/No250137.java
@@ -0,0 +1,12 @@
+package programmers.learn.courses.no30.lessons.lv01.no250137;
+
+public class No250137 {
+
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        // t초 동안 붕대감기, 1초마다 x만큼 체력 회복, t초 연속 성공 시 y만큼 추가 체력 회복
+        // max 회복 가능 체력 존재
+        // 
+        int answer = 0;
+        return answer;
+    }
+}
