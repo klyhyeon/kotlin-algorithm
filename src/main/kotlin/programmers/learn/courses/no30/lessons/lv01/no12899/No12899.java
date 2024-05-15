de3b811685a90f03a6b4de0490e6fb9e47dcae1a
baekho <klyhyeon@gmail.com>
2024-05-15 16:14:56 +0900

    WIP 124 나라

---
+++ src/main/kotlin/programmers/learn/courses/no30/lessons/lv01/no12899/No12899.java
@@ -0,0 +1,18 @@
+package programmers.learn.courses.no30.lessons.lv01.no12899;
+
+public class No12899 {
+
+    public String solution(int n) {
+        // 3진법으로 접근하되 숫자에서 1을 빼고 3의 배수가 나머지 0일 땐 4로 치환해줘야 한다.
+        // 몫이 3이상이면 한번 더 나누는데 그때 나눈 몫에서 -1 해줘야한다.
+        // https://hoons-dev.tistory.com/67
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new No12899().solution(1)
+        );
+    }
+}
