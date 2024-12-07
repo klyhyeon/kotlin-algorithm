688bf8484c8111c4a625b13200492771c21ed5a7
baekho <klyhyeon@gmail.com>
2024-12-08 08:58:08 +0900

    feat: advent code day 1-2

--- src/main/kotlin/advent/Day01.kt
+++ src/main/kotlin/advent/Day01.kt
@@ -6,7 +6,7 @@ import kotlin.math.absoluteValue
 
 class Day01 {
 
-    fun day01() {
+    fun day01Part1() {
         val scanner = Scanner(System.`in`)
         val firstNumbers = mutableListOf<Int>()
         val secondNumbers = mutableListOf<Int>()
@@ -30,8 +30,36 @@ class Day01 {
         scanner.close()
         println("answer: ${distances.sum()}")
     }
+
+    fun day01Part2() {
+        val scanner = Scanner(System.`in`)
+        val firstNumbers = mutableListOf<Int>()
+        val secondNumbers = mutableListOf<Int>()
+        while (scanner.hasNextLine()) {
+            val line = scanner.nextLine()
+            if (line == "") break
+            val twoNumbers = line.split("   ")
+            val firstNum = twoNumbers[0].toInt()
+            val secondNum = twoNumbers[1].toInt()
+            firstNumbers.add(firstNum)
+            secondNumbers.add(secondNum)
+        }
+        firstNumbers.sort()
+        secondNumbers.sort()
+        val similarScores = mutableListOf<Int>()
+        for (i in 0 until firstNumbers.size) {
+            val currentNumber = firstNumbers[i]
+            val similarScore = secondNumbers.count { it == currentNumber }
+            similarScores.add(
+                currentNumber * similarScore
+            )
+        }
+        scanner.close()
+        println("answer : ${similarScores.sum()}")
+    }
 }
 
 fun main() {
-    Day01().day01()
+    Day01().day01Part1()
+    Day01().day01Part2()
 }
