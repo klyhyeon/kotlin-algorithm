d8906a8e6670f6b60051af5dfd2f615a388f0573
yuhyeon <klyhyeon@gmail.com>
2024-12-08 08:50:13 +0900

    feat: advent Day 01 - 1˚

--- src/main/kotlin/advent/Day01.kt
+++ src/main/kotlin/advent/Day01.kt
@@ -1,22 +1,34 @@
 package advent
 
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 class Day01 {
 
     fun day01() {
         val scanner = Scanner(System.`in`)
+        val firstNumbers = mutableListOf<Int>()
+        val secondNumbers = mutableListOf<Int>()
         while (scanner.hasNextLine()) {
             val line = scanner.nextLine()
-            println(line)
+            if (line == "") break
             val twoNumbers = line.split("   ")
             val firstNum = twoNumbers[0].toInt()
             val secondNum = twoNumbers[1].toInt()
-            println("first: $firstNum")
-            println("second: $secondNum")
-            println(firstNum.plus(secondNum))
+            firstNumbers.add(firstNum)
+            secondNumbers.add(secondNum)
+        }
+        firstNumbers.sort()
+        secondNumbers.sort()
+        val distances = mutableListOf<Int>()
+        for (i in 0 until firstNumbers.size) {
+            distances.add(
+                abs(firstNumbers[i].minus(secondNumbers[i]))
+            )
         }
         scanner.close()
+        println("answer: ${distances.sum()}")
     }
 }
 
