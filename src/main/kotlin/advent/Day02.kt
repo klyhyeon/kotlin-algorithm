59fe5699d0df318331b3be903ae4dd50b59deafc
baekho <klyhyeon@gmail.com>
2024-12-15 22:27:55 +0900

    refactor: day01 split functions

--- src/main/kotlin/advent/Day02.kt
+++ src/main/kotlin/advent/Day02.kt
@@ -14,40 +14,10 @@ class Day02 {
             val levels = line.split(" ").map { it.toInt() }
             reports.add(levels)
         }
-        var currentLevel = 0
-        var previousLevel = 0
-        var isOperationNegative = false
-        var levelDistance = 0
         var result = 0
-        var unsafeCount = 0
         for (i in reports.indices) {
-            unsafeCount = 0
-            for (j in 0 until reports[i].size) {
-                currentLevel = reports[i][j]
-                // first level
-                if (j == 0) {
-                    previousLevel = reports[i][j]
-                    continue
-                }
-                val distance = previousLevel.minus(currentLevel)
-                previousLevel = reports[i][j]
-                // all increasing/decreasing
-                if (j == 1) {
-                    isOperationNegative = distance < 0
-                } else {
-                    if (distance < 0 != isOperationNegative) {
-                        unsafeCount++
-                        break
-                    }
-                }
-                // adjacent 1~3
-                levelDistance = abs(distance)
-                if (levelDistance > 3 || levelDistance == 0) {
-                    unsafeCount++
-                    break
-                }
-            }
-            if (unsafeCount == 0) result++
+            if (!isUnsafe(reports[i].toMutableList()))
+                result++
         }
         scanner.close()
         println("answer: $result")
@@ -123,6 +93,6 @@ class Day02 {
 }
 
 fun main() {
-//    Day02().day02Part1()
-    Day02().day02Part2()
+    Day02().day02Part1()
+//    Day02().day02Part2()
 }
